8fe12df833fe7c852d7dfb93bd1ca6ef1a184197
SarinRupp <hemsarin.rupp@gmail.com>
2016-01-19 14:15:56 +0700

    mergh

--- src/khmeracademy/java/org/khmeracademy/app/controllers/elearning/PlaylistRestTemplateController.java
+++ src/khmeracademy/java/org/khmeracademy/app/controllers/elearning/PlaylistRestTemplateController.java
@@ -2,17 +2,22 @@ package org.khmeracademy.app.controllers.elearning;
 
 import java.util.Map;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@RestController
 public class PlaylistRestTemplateController {
 
 	@Autowired
@@ -24,23 +29,27 @@ public class PlaylistRestTemplateController {
 	@Autowired
 	private String WSURL;
 	
-	@RequestMapping(value="/rest/elearning/index" , method = RequestMethod.GET)
+	/*@RequestMapping(value="/rest/elearning/index" , method = RequestMethod.GET)
 	public ResponseEntity<Map<String , Object>> playlistDetail(){
 		HttpEntity<Object> request = new HttpEntity<Object>(header);
 		ResponseEntity<Map> response = rest.exchange(WSURL + "elearning/video/listMainCategoryAndPlaylist", HttpMethod.GET , request , Map.class) ;
 		return new ResponseEntity<Map<String , Object>>(response.getBody() , HttpStatus.OK);
+	}*/
+	
+	@RequestMapping(value="/rest/elearning/playlistdetail/{pid}" , method = RequestMethod.GET)
+	public ResponseEntity<Map<String , Object>> me(@PathVariable(value="pid") String pid){
+		HttpEntity<Object> request = new HttpEntity<Object>(header);
+		ResponseEntity<Map> response = rest.exchange(WSURL + "elearning/playlist/listVideoInPlaylist/" + pid, HttpMethod.GET , request , Map.class) ;
+		return new ResponseEntity<Map<String , Object>>(response.getBody() , HttpStatus.OK);
 	}
 	
-	/*@RequestMapping(value="/rest/elearning/playvideo" , method = RequestMethod.GET)
-	public ResponseEntity<Map<String , Object>> playVideo(
-			@RequestParam(value="v") String vid, 
-			@RequestParam(value="playlist", required=false) String pid){
-		String playlistParam = "";
-		if(pid!=null){
-			playlistParam = "&playlist=" + pid;
-		}
+	
+	/*public ResponseEntity<Map<String , Object>> playVideo(
+			@RequestParam(value="pid") String pid
+			){
+		System.err.println(pid);
 		HttpEntity<Object> request = new HttpEntity<Object>(header);
-		ResponseEntity<Map> response = rest.exchange(WSURL + "elearning/video/playvideo?v=" + vid + playlistParam, HttpMethod.GET , request , Map.class) ;
+		ResponseEntity<Map> response = rest.exchange(WSURL + "elearning/playlist/listVideoInPlaylist?playlistid=" + pid, HttpMethod.GET , request , Map.class) ;
 		return new ResponseEntity<Map<String , Object>>(response.getBody() , HttpStatus.OK);
 	}*/
 	
