f73b9dc7dacdefbbaf72331521b9462ac71b3041
lie111 <lie111@live.com>
2016-01-19 08:43:37 +0700

    xcxc

--- src/khmeracademy/java/org/khmeracademy/app/controllers/precourse/PrecCourseController.java
+++ src/khmeracademy/java/org/khmeracademy/app/controllers/precourse/PrecCourseController.java
@@ -1,8 +1,27 @@
-/**
- * 
- */
-/**
- * @author User
- *
- */
 package org.khmeracademy.app.controllers.precourse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Controller
+public class PrecCourseController {
+	
+	@RequestMapping(value="/list",method= RequestMethod.GET)
+	public String indexPage(ModelMap m){
+		m.addAttribute("msg","message");
+		return "precourse/precourse";
+	}
+	
+	@RequestMapping(value="/admin/precourse",method= RequestMethod.GET)
+	public String preCoursePage(ModelMap m){
+		m.addAttribute("msg","message");
+		return "/admin/precourse";
+	}
+	
+}
+	
+	
