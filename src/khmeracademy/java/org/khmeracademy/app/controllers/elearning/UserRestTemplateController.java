2fd46b5ff6344decb8a1b15ee3d5746d3c8d14a4
chhoin <heistall9@gmail.com>
2016-01-25 17:07:12 +0700

    scsfdsafa

--- src/khmeracademy/java/org/khmeracademy/app/controllers/elearning/UserRestTemplateController.java
+++ src/khmeracademy/java/org/khmeracademy/app/controllers/elearning/UserRestTemplateController.java
@@ -46,7 +46,7 @@ public class UserRestTemplateController {
 									    , @RequestParam(value="item" , required = false , defaultValue="10") int item){
 		
 		HttpEntity<Object> request = new HttpEntity<Object>(header);
-		ResponseEntity<Map> response = rest.exchange(WSURL + "/elearning/history/lituserhistory/"+uid+"?page="+page+"&item="+item, HttpMethod.GET , request , Map.class) ;
+		ResponseEntity<Map> response = rest.exchange(WSURL + "/elearning/history/listuserhistory/"+uid+"?page="+page+"&item="+item, HttpMethod.GET , request , Map.class) ;
 		return new ResponseEntity<Map<String , Object>>(response.getBody() , HttpStatus.OK);
 	}
 	
@@ -57,10 +57,57 @@ public class UserRestTemplateController {
 									    , @RequestParam(value="item" , required = false , defaultValue="10") int item){
 		
 		HttpEntity<Object> request = new HttpEntity<Object>(header);
-		ResponseEntity<Map> response = rest.exchange(WSURL + "/elearning/history/listhistory/"+uid+"/"+vname+"?page="+page+"&item="+item, HttpMethod.GET , request , Map.class) ;
+		ResponseEntity<Map> response = rest.exchange(WSURL + "/elearning/history/searchhistory/"+uid+"/"+vname+"?page="+page+"&item="+item, HttpMethod.GET , request , Map.class) ;
 		return new ResponseEntity<Map<String , Object>>(response.getBody() , HttpStatus.OK);
 	}
 	
+	
+	@RequestMapping(value="/rest/user/profile/removefromhistory/{hid}" , method = RequestMethod.DELETE)
+	public ResponseEntity<Map<String , Object>> removeFromHistory(@PathVariable("hid") String hid){
+		
+		HttpEntity<Object> request = new HttpEntity<Object>(header);
+		ResponseEntity<Map> response = rest.exchange(WSURL + "/elearning/history/deletehistory/"+hid, HttpMethod.DELETE , request , Map.class) ;
+		return new ResponseEntity<Map<String , Object>>(response.getBody() , HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/rest/user/profile/removeallhistory/{uid}" , method = RequestMethod.DELETE)
+	public ResponseEntity<Map<String , Object>> removeAllHistory(@PathVariable("uid") String uid){
+		
+		HttpEntity<Object> request = new HttpEntity<Object>(header);
+		ResponseEntity<Map> response = rest.exchange(WSURL + "/elearning/history/deleteallhistory/"+uid, HttpMethod.DELETE , request , Map.class) ;
+		return new ResponseEntity<Map<String , Object>>(response.getBody() , HttpStatus.OK);
+	}
+	
+	//////////////////video
+	@RequestMapping(value="/rest/user/profile/listuservideo/{uid}" , method = RequestMethod.GET)
+	public ResponseEntity<Map<String , Object>> listUserVideo(@PathVariable("uid") String uid
+										, @RequestParam(value = "page", required = false , defaultValue="1") int page 
+									    , @RequestParam(value="item" , required = false , defaultValue="10") int item){
+		
+		HttpEntity<Object> request = new HttpEntity<Object>(header);
+		ResponseEntity<Map> response = rest.exchange(WSURL + "/elearning/video/user/u/"+uid+"?page="+page+"&item="+item, HttpMethod.GET , request , Map.class) ;
+		return new ResponseEntity<Map<String , Object>>(response.getBody() , HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/rest/user/profile/searchuserhvideo/{uid}/{videoname}" , method = RequestMethod.GET)
+	public ResponseEntity<Map<String , Object>> searchUserVideo(@PathVariable("uid") String uid
+										,@PathVariable("videoname") String vname
+										, @RequestParam(value = "page", required = false , defaultValue="1") int page 
+									    , @RequestParam(value="item" , required = false , defaultValue="10") int item){
+		
+		HttpEntity<Object> request = new HttpEntity<Object>(header);
+		ResponseEntity<Map> response = rest.exchange(WSURL + "/elearning/video/user/u/"+uid+"/name/"+vname+"?page="+page+"&item="+item+"&status=true", HttpMethod.GET , request , Map.class) ;
+		return new ResponseEntity<Map<String , Object>>(response.getBody() , HttpStatus.OK);
+	}
+	
+	
+	@RequestMapping(value="/rest/user/profile/disablevideo/{vid}" , method = RequestMethod.GET)
+	public ResponseEntity<Map<String , Object>> disablevideo(@PathVariable("vid") String vid){
+		
+		HttpEntity<Object> request = new HttpEntity<Object>(header);
+		ResponseEntity<Map> response = rest.exchange(WSURL + "elearning/video/enable/v/"+vid, HttpMethod.PUT , request , Map.class) ;
+		return new ResponseEntity<Map<String , Object>>(response.getBody() , HttpStatus.OK);
+	}
 	
 	
 	
