530d73a13e2b4ff41c915b04606d2bff4a9bb20b
darapenhchet <darapenhchet@gmail.com>
2016-01-27 08:31:51 +0700

    TESTING LOGIN

--- src/khmeracademy/java/org/khmeracademy/app/controllers/AuthenticationController.java
+++ src/khmeracademy/java/org/khmeracademy/app/controllers/AuthenticationController.java
@@ -122,6 +122,7 @@ public class AuthenticationController {
 	    // Create a new session and add the security context.
 	    HttpSession session = request.getSession(true);
 	    session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+	    System.out.println("TESTING ONLY...");
 	}
 
 }
